4459c577ce3a0291e3db138b75e6c1b0482d7fda
Peter Kreppein <pmkreppein@gmail.com>
2019-01-03 23:02:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your code in this file!
+function scuberGreetingForFeet(value) {
+  if (value <= 400) {
+    return "This one is on me!"
+  }
+  else if (value > 2000 && value < 2500) {
+    return "I will gladly take your thirty bucks."
+  }
+  else {
+    return "No can do."
+  }
+};
+function ternaryCheckCity(city) {
+  let nyc = "NYC";
+  const message = city === nyc ? "Ok, sounds good." : "No go.";
+  return message;
+};
+function switchOnCharmFromTip(tip) {
+  let response;
+  switch (tip) {
+    case 'generous':
+      response = "Thank you so much.";
+      break;
+    case 'not as generous':
+      response = "Thank you.";
+      break;
+    default:
+      response = "Bye.";
+      break;
+  };
+  return response;
+};
